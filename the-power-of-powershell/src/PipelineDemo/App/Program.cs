b05f1540e1fcd907fd24c66607595612ace3c4c0
David Mohundro <drmohundro@gmail.com>
2009-04-20 19:20:35 -0500

    Initial commit of presentations!

---
+++ the-power-of-powershell/src/PipelineDemo/App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace PipelineDemo.App
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ParseArgs(args);
+            ParseInputStream();
+        }
+
+        private static void ParseInputStream()
+        {
+            StreamReader reader = new StreamReader(Console.OpenStandardInput());
+            WriteWithColor("---------------------------------");
+            WriteWithColor("Standard Input:");
+            Console.WriteLine(reader.ReadToEnd());
+            WriteWithColor("---------------------------------");
+        }
+
+        private static void ParseArgs(string[] args)
+        {
+            WriteWithColor("---------------------------------");
+            WriteWithColor("Argument list:");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            WriteWithColor("---------------------------------");
+        }
+
+        private static void WriteWithColor(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ the-power-of-powershell/src/troubleshooting-packs/CalcTweaks/TS_CalcTweaks.ps1
@@ -0,0 +1,10 @@
+<#
+TS_CalcTweaks.ps1
+(via http://www.withinwindows.com/2009/01/12/crash-course-on-authoring-windows-7-troubleshooting-packs/)
+ 
+This troubleshooter will check to ensure the coolest calculator
+tweaks are enabled. As of January 2009, there is only one tweak check.
+#>
+ 
+### check to see if programming mode is enabled
+.\TS_ProgMode.ps1 
