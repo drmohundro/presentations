5934e6078f678d2ff8ae28a81b00c09f674fcee4
David Mohundro <david@mohundro.com>
2013-04-15 13:13:50 -0500

    Added PowerShellHoster sample

---
+++ applied-powershell/samples/PowerShellHoster/PowerShellHoster.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main(string[] args) {
+            using (var ps = PowerShell.Create()) {
+                ps.AddScript(@"Get-ChildItem c:\");
+                var result = ps.Invoke();
+                foreach (var r in result) {
+                    Console.WriteLine(r);
+
+                    Console.WriteLine("...which is a...");
+                    Console.WriteLine(r.BaseObject.GetType().FullName);
+
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
